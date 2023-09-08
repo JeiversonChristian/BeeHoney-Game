0ced3d4dbe56c9529da12c50988f5147bcc78a65
JeiversonChristian <jeiversonc@gmail.com>
2023-09-08 13:14:44 -0300

    alterando os elementos e função p/ lógica de sprite

--- objetos.py
+++ objetos.py
@@ -4,10 +4,13 @@ class Objeto_Cenario:
 
     def __init__(self, caminho_img: str, pos_x: int, pos_y: int) -> None:
         
-        self.imagem = pygame.image.load(caminho_img)
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+        self.grupo = pygame.sprite.Group()
+        self.sprite = pygame.sprite.Sprite(self.grupo)
+        self.sprite.imagem = pygame.image.load(caminho_img)
+        self.sprite.rect = self.sprite.image.get_rect()
+        self.sprite.rect[0] = pos_x
+        self.sprite.rect[1] = pos_y
 
     def desenhar(self, tela: pygame.surface.Surface):
         
-        tela.blit(self.imagem, self.pos_x, self.pos_y)
+        self.grupo.draw(tela)
