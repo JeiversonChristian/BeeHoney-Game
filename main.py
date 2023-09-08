7825fe84a0da7d09b6bf66d75a6185666ceea9b9
JeiversonChristian <jeiversonc@gmail.com>
2023-09-08 12:28:24 -0300

    criando a estrutura básica do jogo

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Principal:
+
+    def __init__(self, tamanho_x: int, tamanho_y: int, titulo: str) -> None:
+        
+        self.tela = pygame.display.set_mode([tamanho_x, tamanho_y])
+        self.titulo = pygame.display.set_caption(titulo)
+        self.rodando_game = True
+
+    def desenhar(self) -> None:
+        
+        pass
+
+    def verificar_se_fecha(self) -> None:
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                self.rodando_game = False
+
+    def atualizar_tela(self) -> None:
+
+        while self.rodando_game == True:
+            self.desenhar()
+            self.verificar_se_fecha()
+            pygame.display.update()
+
+game = Principal(320, 640, "BeeHoney")
+game.atualizar_tela()
