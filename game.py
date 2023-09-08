78b9f315e2f75f893cba68c30fb7958169ba5521
JeiversonChristian <jeiversonc@gmail.com>
2023-09-08 15:49:45 -0300

    adicionando um segundo bg e alterando função update

--- game.py
+++ game.py
@@ -6,12 +6,19 @@ class Game:
     def __init__(self) -> None:
         
         self.bg = Objeto_Cenario("assets/bg.png", 0, 0) # background
+        self.bg2 = Objeto_Cenario("assets/bg.png", 0, -640)
         self.muda_cena = False
 
     def desenhar(self, tela: pygame.surface.Surface) -> None:
 
         self.bg.grupo.draw(tela)
+        self.bg2.grupo.draw(tela)
 
     def atualizar_tela(self) -> None:
 
-        pass
+        self.bg.sprite.rect[1] += 1
+        self.bg2.sprite.rect[1] += 1
+        if self.bg.sprite.rect[1] >= 640:
+            self.bg.sprite.rect[1] = 0
+        if self.bg2.sprite.rect[1] >= 0:
+            self.bg2.sprite.rect[1] = -640
