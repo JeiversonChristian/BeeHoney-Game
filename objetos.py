5f96d2c3a696ab31d6ae9f7f74d5c0a9e7227367
JeiversonChristian <jeiversonc@gmail.com>
2023-09-08 17:43:28 -0300

    controlando animação da aranha por ticks

--- objetos.py
+++ objetos.py
@@ -11,13 +11,17 @@ class Objeto_Cenario:
         self.sprite.rect[0] = pos_x
         self.sprite.rect[1] = pos_y
         self.frame = 1
+        self.tick = 0
 
     def desenhar_objeto(self, tela: pygame.surface.Surface) -> None:
         
         self.grupo.draw(tela)
 
     def anime(self) -> None:
-        self.frame += 1
+        self.tick += 1
+        if self.tick >=30:
+            self.tick = 0
+            self.frame += 1
         if self.frame > 4:
             self.frame = 1
         self.sprite.image = pygame.image.load("assets/spider"+str(self.frame)+".png")
