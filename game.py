c0e681bde0345e38f099a75782f24d6f44631426
JeiversonChristian <jeiversonc@gmail.com>
2023-09-08 17:13:04 -0300

    adicionando animação da aranha

--- game.py
+++ game.py
@@ -8,13 +8,20 @@ class Game:
         self.bg = Objeto_Cenario("assets/bg.png", 0, 0) # background
         self.bg2 = Objeto_Cenario("assets/bg.png", 0, -640)
         self.muda_cena = False
+        self.aranha = Objeto_Cenario("assets/spider1.png", 200, 200)
 
     def desenhar(self, tela: pygame.surface.Surface) -> None:
 
         self.bg.grupo.draw(tela)
         self.bg2.grupo.draw(tela)
+        self.aranha.grupo.draw(tela)
 
     def atualizar_tela(self) -> None:
+
+        self.mova_bg()
+        self.aranha.anime()
+
+    def mova_bg(self) -> None:
 
         self.bg.sprite.rect[1] += 1
         self.bg2.sprite.rect[1] += 1
